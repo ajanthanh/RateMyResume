32cd48b4d2b2619a6e983b872525f7d5d76db4c6
Ajanthan Hariharan <ajanthanh@gmail.com>
2016-01-06 10:46:35 -0500

    created a end user account sign up

--- account/views.py
+++ account/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {})
+    title = "Welcome"
+    # if request.user.is_authenticated():
+    #     title = "My Title %s" % (request.user)
+    form = SignUpForm(request.POST or None)
+
+    context = {
+        "title": title,
+        "form": form
+    }
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        print(instance)
+        context ={
+            "title": "Thank you"
+        }
+
+    return render(request, "home.html", context)
