c2defac8196b898d21650c36e39bef45c6110abf
Safwan Alam <safwan_alam@hotmail.com>
2016-01-13 21:32:08 -0800

    added file_name parameter

--- resume/models.py
+++ resume/models.py
@@ -9,7 +9,7 @@ def get_upload_file_name(instance, filename):
 
 
 class Resume(models.Model):
-    # file_name = models.CharField(max_length=120, blank=False, null=True)
+    file_name = models.CharField(max_length=120, blank=False, null=True)
     thumbnail = models.FileField(upload_to=get_upload_file_name, blank=True, null=True)
     # timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
